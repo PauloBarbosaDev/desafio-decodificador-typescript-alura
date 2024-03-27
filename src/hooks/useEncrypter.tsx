012a6e54ef1ecf7f14cc9952ee57d0bda55930a1
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-03-27 05:00:57 -0300

    Build project

--- src/hooks/useEncrypter.tsx
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import { EncrypterContext } from '../context/EncrypterContext';
-
-export default function useEncrypter() {
-  return useContext(EncrypterContext);
-}
